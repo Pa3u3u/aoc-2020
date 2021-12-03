d36b0bf39bf4bcfe96a5ec683547de1f5215f29f
Roman Lacko <xlacko1@fi.muni.cz>
2021-12-03 11:42:42 +0100

    01: Use Toolbox

--- 01/sweep.ml
+++ 01/sweep.ml
@@ -1,25 +1,9 @@
 open Printf
 
-let file_as_seq file_name =
-    let unfolder handle =
-        try let line = input_line handle in
-            Some (line, handle)
-        with _ ->
-            close_in_noerr handle;
-            None
-
-    and file = open_in file_name in
-        Seq.unfold unfolder file
-
 
 let count_increases (state, counter) next = match (state, next) with
     | (None, next) -> (Some next, counter)
     | (Some prev, next) -> (Some next, counter + if prev < next then 1 else 0)
-
-
-let read_int str =
-    try Some (int_of_string str)
-    with _ -> None
 
 
 let sum = List.fold_left (+) 0
@@ -50,10 +34,11 @@ let () =
     if Array.length Sys.argv - 1 <> 1 then begin
         Printf.eprintf "usage: %s FILE\n" Sys.argv.(0);
         exit 0;
-    end else
-        file_as_seq Sys.argv.(1)
-            |> Seq.filter_map read_int
-            |> transform_data 3
-            |> Seq.fold_left count_increases (None, 0)
-            |> snd
-            |> printf "%d\n"
+    end;
+
+    Toolbox.File.as_seq Sys.argv.(1)
+        |> Seq.filter_map Toolbox.Num.parse_int
+        |> transform_data 3
+        |> Seq.fold_left count_increases (None, 0)
+        |> snd
+        |> printf "%d\n"
