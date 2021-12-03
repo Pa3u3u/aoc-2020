d4f3c2e9ad1bba6d6d69fcc9531863b1ddbe639a
Roman Lacko <xlacko1@fi.muni.cz>
2021-12-03 11:41:05 +0100

    Add library Toolbox with common functions from 01 and 02

---
+++ toolbox/toolbox.ml
@@ -0,0 +1,21 @@
+module File = struct
+
+    let as_seq file_name =
+        let unfolder handle =
+            try let line = input_line handle in
+                Some (line, handle)
+            with _ ->
+                close_in_noerr handle;
+                None
+        and file = open_in file_name in
+            Seq.unfold unfolder file
+
+end
+
+module Num = struct
+
+    let parse_int str =
+        try Some (int_of_string str)
+        with _ -> None
+
+end
