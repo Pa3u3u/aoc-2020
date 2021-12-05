48707a24b8b2ab2832acae8ecfeb754cd85ee06e
Roman Lacko <xlacko1@fi.muni.cz>
2021-12-05 15:37:27 +0100

    toolbox: Add Range module

--- toolbox/toolbox.ml
+++ toolbox/toolbox.ml
@@ -38,16 +38,53 @@ module List = struct
 end
 
 
-let both f (x, y) =
-    (f x, f y)
+module Range = struct
+    type 'a t = 'a * 'a * ('a -> bool)
 
 
-let fork f g a =
-    (f a, g a)
+    let sign = function
+        | n when n > 0 ->  1
+        | n when n < 0 -> -1
+        | _ -> 0
 
+
+    let make_step (a: 'a) (z: 'a) (step: 'a): 'a t =
+        assert (step != 0);
+        let f = if step > 0 then Fun.flip (<) z else Fun.flip (>) z in
+        (a, step, f)
+
+
+    let make (a: 'a) (z: 'a): 'a t =
+        let step = sign (z - a) in
+        assert (step != 0);
+        make_step a z step
+
+
+    let make_inc (a: 'a) (z: 'a): 'a t =
+        let step = sign (z - a) in
+        assert (step != 0);
+        make_step a (z + step) step
+
+
+    let as_seq ((a, step, f): 'a t): 'a Seq.t =
+        let unfolder = function
+            | n when f n -> Some (n, n + step)
+            | _ -> None in
+        Seq.unfold unfolder a
+
+
+    let as_list (r: 'a t): 'a list =
+        as_seq r |> List.of_seq
+end
+
+
+let both f (x, y) = (f x, f y)
+let fork f g a = (f a, g a)
 
 let curry f a b = f (a, b)
 let uncurry f (a, b) = f a b
 
 let first f (a, b) = (f a, b)
 let second f (a, b) = (a, f b)
+
+let peek f v = f v; v
