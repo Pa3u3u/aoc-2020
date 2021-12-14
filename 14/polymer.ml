3b42123a6860805dd332ea61b20cd98503ebfa1a
Roman Lacko <xlacko1@fi.muni.cz>
2021-12-14 09:17:18 +0100

    14: Add solution (part 1)

---
+++ 14/polymer.ml
@@ -0,0 +1,96 @@
+open Printf
+open Toolbox
+open Toolbox.Operators
+
+exception Invalid_input of string
+
+module Rule = struct
+    type t =  (char * char) * char
+end
+
+
+module Polymer = struct
+    let expand_once (pseq: char Seq.t) (rules: Rule.t list) =
+        let rec apply a b = function
+            | ((x, y), c)::_ when a = x && b = y -> [a; c]
+            | _::rest -> apply a b rest
+            | [] -> [a] in
+        let term = Char.chr 0 in
+        let unfolder ((a, s): (char * char Seq.t)) = match s () with
+            | Cons (b, rest) -> Some (apply a b rules, (b, rest))
+            | Nil when a != term -> Some ([a], (term, Seq.empty))
+            | Nil -> None in
+
+        match pseq () with
+            | Cons (v, rest) -> Seq.unfold unfolder (v, rest)
+            | Nil -> Seq.empty
+
+    let fold_expand_once n poly =
+        printf "# Expansion %d\n" (n + 1); Stdlib.(flush stdout);
+        expand_once poly >> Seq.flat_map List.to_seq
+
+    let expand (n: int) (poly: char Seq.t) (rules: Rule.t list): char Seq.t =
+        List.fold_left (fun p n -> fold_expand_once n p rules) poly (0 &-- n)
+
+    module CharMap = Map.Make(struct
+        type t = char
+        let compare = Char.compare
+    end)
+
+    type stat_t = int CharMap.t
+
+    let statistics: char Seq.t -> stat_t =
+        let update_stats stats c =
+            let inc_count = function
+                | None -> Some 1
+                | Some n -> Some (n + 1) in
+            CharMap.update c inc_count stats in
+        Seq.fold_left update_stats (CharMap.empty)
+
+    let select_elements: stat_t -> ((char * int) * (char * int)) =
+        let select_max_min l =
+            (List.hd l, List.rev l |> List.hd) in
+        CharMap.bindings >> List.sort (fun (_, x) (_, y) -> Int.compare y x)
+            >> select_max_min
+end
+
+
+let parse_input: string Seq.t -> (char list * Rule.t list) =
+    let parse_header (seq: string Seq.t): (char list * string Seq.t) = match seq () with
+        | Cons (str, rest) -> (String.to_chars str, rest)
+        | Nil -> raise (Invalid_input "Empty input") in
+
+    let parse_rules: string Seq.t -> Rule.t list =
+        let regex = Str.regexp "\\([A-Z]+\\) -> \\([A-Z]\\)" in
+        let unpack = function
+            | [a; b] -> (a, b)
+            | _ -> raise (Invalid_input "Invalid number of resources in the rule") in
+        let parse_rule line =
+            if not (Str.string_match regex line 0) then
+                None
+            else
+                Some (Str.matched_group 1 line |> String.to_chars |> unpack,
+                        Str.matched_group 2 line |> Fun.flip String.get 0) in
+        Seq.filter_map parse_rule >> List.of_seq in
+
+    parse_header >> second parse_rules
+
+
+let () =
+    let argc = Array.length Sys.argv - 1 in
+
+    if argc < 1 || argc > 2 then begin
+        Printf.eprintf "usage: %s FILE [N]\n" Sys.argv.(0);
+        exit 1;
+    end;
+
+    let repeats = if argc == 1 then 10 else Num.parse_int_exn Sys.argv.(2) in
+
+    File.as_seq Sys.argv.(1)
+        |> parse_input
+        |> first List.to_seq
+        |> uncurry (Polymer.expand repeats)
+        |> Polymer.statistics
+        |> Polymer.select_elements
+        |> peek (fun ((a, ac), (b, bc)) -> printf "# (%c: %d) (%c: %d)\n" a ac b bc)
+        |> both snd |> uncurry (-) |> printf "%d\n"
