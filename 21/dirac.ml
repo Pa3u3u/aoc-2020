4b21c147525eb408414ca6c6077e2a99e071bd50
Roman Lacko <xlacko1@fi.muni.cz>
2021-12-27 18:02:30 +0100

    21: Add solution (part 2)

--- 21/dirac.ml
+++ 21/dirac.ml
@@ -19,72 +19,101 @@ end
 module Conf = struct
     let rolls = 3
     let plan_size = 10
-    let win = 1000
+    let win = 21
 end
 
-class dice = object
-    val mutable next = 0
-    val mutable rolls = 0
+module Dice = struct
+    let condense l =
+        let rec condense' (v, c) = function
+            | [] -> [(v, c)]
+            | w::xs when v = w -> condense' (v, c + 1) xs
+            | w::xs -> (v, c) :: condense' (w, 1) xs in
+        condense' (List.hd l, 1) (List.tl l)
 
-    method roll =
-        let r = next + 1 in
-            rolls <- rolls + 1;
-            next <- (next + 1) mod 100;
-            r
 
-    method rolls =
-        rolls
+    class dice = object
+        val rolls =
+            List.product (List.product (1 &-- 4) (1 &-- 4)) (1 &-- 4)
+                |> List.map (fun ((a, b), c) -> a + b + c)
+                |> List.sort Int.compare
+                |> condense
+
+        method rolls =
+            rolls
+    end
 end
 
-class player (start: int) = object (self)
-    val mutable score = 0
-    val mutable pos = start - 1
+class dice = Dice.dice
 
-    method private add_score n =
-        score <- score + n
+module Player = struct
+    type t = int * int
 
-    method move rolls =
-        let total = List.sum rolls in
-        pos <- (pos + total) mod Conf.plan_size;
-        self#add_score (pos + 1);
+    let create p = (0, p - 1)
 
-    method pos =
-        pos + 1
+    let score = fst
+    let pos = snd
 
-    method score =
-        score
+    let move ((s, p): t) n =
+        let new_p = (p + n) mod Conf.plan_size in
+        (s + new_p + 1, new_p)
+
+    let won ((s, _): t) = s >= Conf.win
 end
+
+type player = Player.t
 
 module Game = struct
-    type t = player * player
+    type round =
+        | P1 | P2
+
+    let next_round = function
+        | P1 -> P2
+        | P2 -> P1
+
+    type t = player * player * round
 
     let create: int list -> t =
-        List.map (new player) >> function
-            | [p1; p2] -> (p1, p2)
+        List.map Player.create >> function
+            | [p1; p2] -> (p1, p2, P1)
             | _ -> raise (Failure "Invalid number of players")
 
-    let play (players: t): (int * int * dice * t) =
-        let count = 2 in
-        let rec play' round pidx d (p, q) =
-            let next_round = round + (pidx + 1) / count
-            and next_pidx = (pidx + 1) mod count in
+    let equal (p1, q1, f1) (p2, q2, f2) =
+        f1 = f2 && p1 = p2 && q1 = q2
 
-            printf "# Round %d player %d: " (round + 1) (pidx + 1);
-            0 &-- Conf.rolls
-                |> List.map (fun _ -> d#roll) |> p#move
-                |> Fun.peek (fun _ -> printf "move to %d, score %d\n" p#pos p#score)
-                |> function
-                    | _ when p#score >= Conf.win -> (round, pidx, d, (p, q))
-                    | _ -> play' next_round next_pidx d (q, p) in
+    let advance dice ((p, q, f), c) =
+        let do_roll (wins, games) (move, card) =
+            let new_p = Player.move p move in
+            if Player.won new_p
+            then wins + (c * card), games
+            else wins, ((q, new_p, next_round f), c * card) :: games in
 
-        play' 0 0 (new dice) players
+        dice#rolls |> List.fold_left do_roll (0, []) |> function
+            | (w, g) when f = P1 -> (w, 0, g)
+            | (w, g) -> (0, w, g)
 
-    let result (round, player, dice, (winner, loser)) =
-        printf "# Player %d won in round %d\n" player round;
-        printf "#   Winner: position %d score %d\n" winner#pos winner#score;
-        printf "#   Loser:  position %d score %d\n" loser#pos loser#score;
-        printf "# Dice has been rolled %d times\n" dice#rolls;
-        printf "%d\n" (dice#rolls * loser#score)
+    let join games =
+        let rec join_game all (g, c) = match all with
+            | [] -> [(g, c)]
+            | (g', c')::rest when equal g g' -> (g', c + c')::rest
+            | w::rest -> w :: join_game rest (g, c) in
+        List.fold_left join_game games
+
+    let play (game: t): (int * int) =
+        let rec play' (wp, wq) d l =
+            (*printf "# Remaining %d games\n" (List.length l);
+            Stdlib.(flush stdout); *)
+            match l with
+            | [] -> (wp, wq)
+            | g::rest ->
+                let (dp, dq, new_games) = advance d g in
+                play' (wp + dp, wq + dq) d (join rest new_games) in
+        play' (0, 0) (new dice) [(game, 1)]
+
+    let result (wp, wq) =
+        let max a b = if a > b then a else b in
+        printf "# Player 1 won %d times\n" wp;
+        printf "# Player 2 won %d times\n" wq;
+        printf "%d\n" (List.fold_left max 0 [wp; wq])
 end
 
 let () =
